4e203026e1bec39c8bb166f9c5f7153eb68ac260
Amrit Verma <amrito@ad.unc.edu>
2023-01-20 10:12:15 -0500

    Input practice

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking the user for input"""
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + " good morning!")
+print("Your are the best programmer ever, " + user_name)
