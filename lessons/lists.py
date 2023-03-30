e290e266a5dc183e65f087ab02c8d5d726f65c8d
Amrit Verma <amrito@ad.unc.edu>
2023-03-30 14:33:15 -0400

    lists

---
+++ lessons/lists.py
@@ -0,0 +1,9 @@
+"""Practice with lists."""
+
+grocery_list: list[str] = list()
+grocery_list.append("bananas")
+grocery_list.append("milk")
+grocery_list.append("bread")
+grocery_list.append("bananas")
+grocery_list[3] = "cereal"
+print(grocery_list)
